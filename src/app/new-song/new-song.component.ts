42db0938875d8b1129c5383e6644d67077d197bf
Garrett Manley <garrett.manley@perficient.com>
2019-08-16 15:56:50 -0500

    setup styling basics

--- src/app/new-song/new-song.component.ts
+++ src/app/new-song/new-song.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'karaoke-new-song',
   templateUrl: './new-song.component.html',
-  styleUrls: ['./new-song.component.scss']
+  styleUrls: ['./new-song.component.scss'],
 })
 export class NewSongComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  addNewSongClicked() {
+    console.log(`Clicked`);
   }
-
 }
